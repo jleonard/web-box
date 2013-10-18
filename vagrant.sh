d30de3b8b7b07379c555a36b88214f153757ec23
John Leonard <thejohnleonard@gmail.com>
2013-10-18 16:20:45 -0400

    Update vagrant.sh
    moving npm installation ahead of phantom and casper to get things moving faster during the install.

--- vagrant.sh
+++ vagrant.sh
@@ -9,12 +9,12 @@ sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:chris-lea/node.js
-#sudo apt-get update
 sudo apt-get install -y nodejs
 
 sudo apt-get install -y git
 
-#sudo apt-get update
+npm install -g yo grunt-cli bower docpad express node-dev forever huxley selenium-server
+
 sudo apt-get install -y build-essential chrpath git-core libssl-dev libfontconfig1-dev
 git clone git://github.com/ariya/phantomjs.git
 cd phantomjs
@@ -25,14 +25,6 @@ git clone git://github.com/n1k0/casperjs.git
 cd casperjs
 git checkout tags/1.0.2
 ln -sf `pwd`/bin/casperjs /usr/local/bin/casperjs
-
-#sudo apt-get install -y firefox
-
-#sudo apt-get install -y openjdk-6-jre
-
-#sudo apt-get install -y graphicsmagick
-
-npm install -g yo grunt-cli bower docpad express node-dev forever huxley selenium-server
 
 sudo apt-get -y install tree
 

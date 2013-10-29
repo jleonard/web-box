3227ec0d76d090599a0166a5695692b932183c5e
jleonard <thejohnleonard@gmail.com>
2013-10-29 09:02:30 -0400

    building node from joyent source

--- vagrant.sh
+++ vagrant.sh
@@ -6,16 +6,21 @@ rm -rf /var/www
 ln -fs /vagrant /var/www
 
 sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential curl chrpath git-core openssl libssl-dev libfontconfig1-dev
 
 sudo apt-get install -y git
 
+git clone https://github.com/joyent/node.git
+cd node
+git checkout v0.10.21
+./configure
+make
+sudo make install
+
+curl http://npmjs.org/install.sh | sudo sh
+
 npm install -g yo grunt-cli bower docpad express node-dev forever huxley selenium-server
 
-sudo apt-get install -y build-essential chrpath git-core libssl-dev libfontconfig1-dev
 git clone git://github.com/ariya/phantomjs.git
 cd phantomjs
 git checkout 1.9
